e83725f450c0f55bf6f8d823f53eda1b72762f4a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-07-16 18:43:43 -0700

    sage_setup.command.sage_install.sage_install: Rename to sage_install_and_clean, update use it in src/setup.py; refactor through new class sage_install

--- src/setup.py
+++ src/setup.py
@@ -177,6 +177,5 @@ code = setup(name = 'sage',
       cmdclass = dict(build=sage_build,
                       build_cython=sage_build_cython,
                       build_ext=sage_build_ext,
-                      #install=sage_install
-                      ),
+                      install=sage_install),
       ext_modules = cython_modules)
